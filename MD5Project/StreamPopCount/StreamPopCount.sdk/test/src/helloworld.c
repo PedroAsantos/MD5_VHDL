2eb20b381c529a0f3a43fd172bcaae877d591b24
Miguel Maia <maiamiguel@ua.pt>
2018-07-03 23:50:28 +0100

    block bug fixed

--- MD5Project/StreamPopCount/StreamPopCount.sdk/test/src/helloworld.c
+++ MD5Project/StreamPopCount/StreamPopCount.sdk/test/src/helloworld.c
@@ -13,7 +13,7 @@ int main(){
     init_platform();
 
     xil_printf("\n\r%sHello Stream Coprocessor\n\r");
-
+/*
     char array[5];
 	unsigned int ch;
 
@@ -24,29 +24,30 @@ int main(){
 	int numberOfBlocks = (int)ceil(initial_len/448.0);
 
 
-	char *block;
+	char *block;*/
 	/* Define the amount of memory required, 64 * 8=512 -> it will reserve 512 bits */
 	size_t length = 64;
 
-
+/*
 	block = (char *)calloc(length, sizeof(char));
 
 	/* Check to see if we were successful */
+	/*
 	if (block == NULL)
 	{
 	    /* We were not so display a message */
-	    printf("Could not allocate required memory\n");
+	/*    printf("Could not allocate required memory\n");
 
 	    /* And exit */
-	    exit(1);
+	/*    exit(1);
 	}
   char *subBlock;
   /* Define the amount of memory required, 64 * 8=512 -> it will reserve 512 bits */
 
-  subBlock = (char *)calloc(4, sizeof(char));
+  /*subBlock = (char *)calloc(4, sizeof(char));
 
 	int i;
-  int c;
+	int c;
 	for( i = 0; i < numberOfBlocks; i = i + 1 ){
 	    memset(block, 0, 512);
 	    if(i==numberOfBlocks-1){
@@ -71,30 +72,41 @@ int main(){
 	    }
 
 	 }
-	  char *subBlockTemp;
+*/	  char *subBlockTemp;
 	  /* Define the amount of memory required, 64 * 8=512 -> it will reserve 512 bits */
 
 	  subBlockTemp = (char *)calloc(4, sizeof(char));
+
 	  char *blockTemp;
 	  	/* Define the amount of memory required, 64 * 8=512 -> it will reserve 512 bits */
 	  size_t lengthTemp = 64;
+	  xil_printf("\nsubBlock______asd______________>__>_%s", subBlockTemp);
 
 
 	  blockTemp = (char *)calloc(lengthTemp, sizeof(char));
-	  char *basePtr=blockTemp;
-	  for(i=0;i<64;i++){
-		  blockTemp = 'a';
-		  blockTemp=blockTemp+8;
+	  //char *basePtr=blockTemp;
+	 // for(i=0;i<64;i++){
+	 int i;
+	  for(i=0;i<lengthTemp;i++){
+		  //blockTemp = 'a';
+		 //blockTemp=blockTemp+8;
+		  blockTemp[i]='a'+(i%26);
+
 	  }
-
-
+	  xil_printf("\nbasePtr_%s", blockTemp);
+	  xil_printf("\nsubBlock____________________>__>_%s", subBlockTemp);
+	  int c;
 	  for(c=0;c<16;c=c+1){
-	     memcpy(subBlockTemp,basePtr+(c*32),32);
+	     memcpy(subBlockTemp,blockTemp+(c*4),4);
+	     xil_printf("\nsubBlock size -> %d", (int) (sizeof(subBlockTemp)));
 	     putfsl(subBlockTemp, 0);
+	     xil_printf("\nsubBlock_%s", subBlockTemp);
 	  }
+
+
 	//putfsl("", 0);
 	getfsl(r, 0);
-	xil_printf("\n\r_%08x", r);
+	xil_printf("\n%08x", r);
 
     xil_printf("\n\rDone!");
 
